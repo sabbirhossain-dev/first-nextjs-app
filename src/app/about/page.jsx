dfd4d35a740d269464a5226c9388bb33d0316e93
Md Sabbir Hossain <hossainsabbir309@gmail.com>
2026-09-17 23:04:15 +0600

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      about page
+      <div>
+        <p>hello</p>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
